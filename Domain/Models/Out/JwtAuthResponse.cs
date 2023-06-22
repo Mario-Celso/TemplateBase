90f2228a9cd97f3e60e2a49b13b83cda142534f2
Mario-Celso <marioc_almeida@outlook.com>
2023-06-21 21:38:31 -0300

    retorno do json de token

---
+++ Domain/Models/Out/JwtAuthResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Out
+{
+    [Serializable]
+    public class JwtAuthResponse
+    {
+        public string token { get; set; }
+        public string user_name { get; set; }
+        public int expires_in { get; set; }
+    }
+}
